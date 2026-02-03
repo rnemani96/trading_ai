5beea3c714c19b8b0d20f83a2d9ace84e049faf1
Rajesh Nemani <rnemani96@gmail.com>
2026-02-03 11:25:54 +0530

    Implement test for LongTermInvestEnv step
    Add unit test for LongTermInvestEnv step method

---
+++ tests/test_long_term_env.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from src.envs.long_term_env import LongTermInvestEnv
+
+def test_env_step():
+    df = pd.DataFrame({
+        "Close": [100, 102, 101],
+        "rsi": [40, 45, 50],
+        "ema20": [99, 100, 101],
+        "ema50": [98, 99, 100],
+    })
+
+    env = LongTermInvestEnv(df)
+    obs = env.reset()
+
+    obs, reward, done, _ = env.step([0.5])
+
+    assert len(obs) == 4
+    assert isinstance(reward, float)
